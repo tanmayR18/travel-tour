cac2be47344927aee008cace459d8e616c8b9dcb
Tanmay Rane <135257857+tanmayR18@users.noreply.github.com>
2023-06-10 00:57:04 +0530

    resources added

---
+++ src/components/Card.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+
+function Card({id, name, info, image, price, removeTour}){
+    const [readmore,setReadmore] = useState(false);
+    const description = readmore ? info : `${info.substring(0,200)}....`
+    function readmoreHandler(){
+        setReadmore(!readmore);
+    }
+    
+    return(
+        <div className='card'>
+            <img src={image} className='image' alt=''></img>
+            <div className='tour-info'>
+                <div className='tour-detail'>
+                    <h4 className='tour-price'>₹ {price}</h4>
+                    <h4 className='tour-name'>{name}</h4>
+                </div>
+                <div className='description'>
+                    {description}
+                    <span className='read-more' onClick={readmoreHandler}>
+                        { readmore ? 'Show Less' : 'Read More' }
+                    </span>
+                </div>
+            </div>
+            <button className='btn-red' onClick={()=> removeTour(id)}>
+                Not Intrested
+            </button>
+        </div>
+    )
+}
+
+export default Card;
